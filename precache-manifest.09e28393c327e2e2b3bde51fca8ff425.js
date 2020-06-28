7938eed1b1ccab03cb6859a901203d6bd8decc86
Carlos Alberto <carlosealberto@hotmail.com.br>
2020-06-27 22:52:26 -0300

    Updates

---
+++ precache-manifest.09e28393c327e2e2b3bde51fca8ff425.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5efd31600af9f30947fcc4393ff15a",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "6cadf20db7b0b1f3a00f",
+    "url": "/cv/static/css/main.979827ca.chunk.css"
+  },
+  {
+    "revision": "ed3d934d4418143323e4",
+    "url": "/cv/static/js/2.398d51f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv/static/js/2.398d51f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cadf20db7b0b1f3a00f",
+    "url": "/cv/static/js/main.416f3f14.chunk.js"
+  },
+  {
+    "revision": "d70ab44d29425c988055",
+    "url": "/cv/static/js/runtime-main.003c8c88.js"
+  },
+  {
+    "revision": "ab1d67f26560a8eaec35ccab13515462",
+    "url": "/cv/static/media/me.ab1d67f2.jpg"
+  }
+]);
